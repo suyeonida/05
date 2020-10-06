5dd09c1715da27b62ebace2d5e550d24e6fd17a0
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-10-06 16:15:19 +0900

    03

--- main.c
+++ main.c
@@ -5,17 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	int num=0;
+	char c; 
 	
-	printf("정수 하나를 입력하시오.:");
-	scanf("%i", &i); 
-	
-	if (i>=0)
-	    printf("절댓값은 %d 입니다.\n", i);
-	
-	else 
-	    printf("절댓값은 %d 입니다.\n", (-1)*i);
- 
+	printf("input a string:");
+	while( (c=getchar()) !='\n'){
+		if (c>='0' && c<='9'){
+			num++;
+		}
+	}
+	printf("the number of digits is %i\n", num);
 	return 0;
 	
 }
